173ead6e503cfdfd2c8b554efa777244db39bcf3
Dani88alv <60398239+Dani88alv@users.noreply.github.com>
2022-05-17 20:35:19 +0200

    Añadir método captura

--- src/pokemon/Entrenador.java
+++ src/pokemon/Entrenador.java
@@ -2,6 +2,11 @@ package pokemon;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+import combate.Estado;
+import combate.Tipo;
 
 public class Entrenador {
 
@@ -22,6 +27,20 @@ public class Entrenador {
 		equipo1.add(poke2);
 		equipo1.add(poke3);
 		equipo1.add(poke4);
+
+		if (poke1 != null) {
+			cajaPokemon.add(poke1);
+		}
+		if (poke2 != null) {
+			cajaPokemon.add(poke2);
+		}
+		if (poke3 != null) {
+			cajaPokemon.add(poke3);
+		}
+		if (poke4 != null) {
+			cajaPokemon.add(poke4);
+		}
+
 	}
 
 	public String getNombre() {
@@ -50,11 +69,82 @@ public class Entrenador {
 
 	public Pokemon sacarPokemon() {
 		for (int i = 0; i < getEquipo1().size(); i++) {
-			if (getEquipo1().get(i).getPuntosSaludCombate() <= 0) {
+			if (getEquipo1().get(i).getPuntosSaludCombate() > 0) {
 				return getEquipo1().get(i);
 			}
 		}
 		return null;
+	}
+
+	public Pokemon generarPokemon() {
+		Random rando = new Random();
+		int id = rando.nextInt(10) + 1;
+		int ps = rando.nextInt(99) + 1;
+		int atFi = rando.nextInt(99) + 1;
+		int deFi = rando.nextInt(99) + 1;
+		int atEs = rando.nextInt(99) + 1;
+		int deEs = rando.nextInt(99) + 1;
+		int vel = rando.nextInt(99) + 1;
+		int nivel = rando.nextInt(10) + 1;
+		Pokemon pokemon = new Pokemon(id, "test", ps, atFi, deFi, atEs, deEs, vel, nivel, 0, Estado.SIN_ESTADO,
+				Tipo.CRYO, null, null, null, null);
+		return pokemon;
+	}
+
+	public void capturar() {
+		Random rando = new Random();
+		Scanner sc = new Scanner(System.in);
+		int eleccion = 1; // elección del menú
+		int eleccionCaptura; // elección de capturar o huir
+		int contadorTurnos = 0; // contador de turnos disponibles
+		int contadorTurnos2 = 0; // condición de preguntar si seguir capturando
+		int acierto; // probabilidad de captura
+
+		while (eleccion == 1 || eleccion == 2) {
+
+			Pokemon objetivo = generarPokemon();
+
+			System.out.println("Pokemon: " + objetivo.getIdEspecie());
+
+			System.out.println("1 (capturar), 2 (aleatorizar), 3 (salir)");
+			eleccion = sc.nextInt();
+
+			if (eleccion == 1) {
+				while (contadorTurnos < 3) {
+					contadorTurnos++;
+					contadorTurnos2++;
+					acierto = rando.nextInt(99);
+					if (acierto == 0) {
+						System.out.println("¡Captura completada!");
+						cajaPokemon.add(objetivo);
+						contadorTurnos = 3;
+						contadorTurnos2 = 3;
+					} else {
+						System.out.println("¡Ha fallado!");
+					}
+
+					if (contadorTurnos2 < 3) { // Condición de seguir preguntando
+						System.out.println("1 (seguir capturando), 2 (huir)");
+						eleccionCaptura = sc.nextInt();
+						if (eleccionCaptura == 2) {
+							contadorTurnos = 3;
+						}
+					}
+
+				}
+				contadorTurnos = 0;
+				contadorTurnos2 = 0;
+			}
+
+			if (eleccion == 2) {
+				eleccion = 2;
+			}
+			if (eleccion == 3) {
+				eleccion = 3;
+			}
+
+		}
+		sc.close();
 	}
 
 }
