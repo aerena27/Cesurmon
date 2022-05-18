b1396c4bb36010bca24571ff4ac54265ed5a1e5c
Dani88alv <60398239+Dani88alv@users.noreply.github.com>
2022-05-18 21:27:07 +0200

    Configuración inicial Javafx

--- src/modelo/App.java
+++ src/modelo/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("./vistas/menuPrincipal.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("../vistas/menuPrincipal.fxml"));
             Scene scene = new Scene(root);
 
             primaryStage.setScene(scene);
