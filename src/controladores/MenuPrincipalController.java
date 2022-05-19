c19a9ab464b5811ad47e1cbe391a130743ca3aaf
Dani88alv <60398239+Dani88alv@users.noreply.github.com>
2022-05-19 15:58:35 +0200

    Primer cambio de ventanas

--- src/controladores/MenuPrincipalController.java
+++ src/controladores/MenuPrincipalController.java
@@ -5,20 +5,34 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class MenuPrincipalController implements Initializable {
-
-    @FXML
-    private Button btnHolaMundo; // %sayHelloWorld
 
     @FXML
     private TextField txtfield;
 
     @FXML
-    private void sayHelloWorld(ActionEvent event) { // @btnHolaMundo
+    private Button btnCombatir; // entrarMenuCombate
+
+    @FXML
+    private void entrarMenuCombate() throws Exception { // btnCombatir
+        Parent root = FXMLLoader.load(getClass().getResource("../vistas/menuCombatePrincipal.fxml"));
+        Stage window = (Stage) btnCombatir.getScene().getWindow();
+        window.setScene(new Scene(root));
+    }
+
+    @FXML
+    private Button btnHolaMundo; // sayHelloWorld
+
+    @FXML
+    private void sayHelloWorld(ActionEvent event) { // btnHolaMundo
         System.out.println("Hola mundo");
     }
 
